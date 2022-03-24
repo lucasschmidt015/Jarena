7261524fe3a7e3f44f2209f116259ff00c16ea0b
Lucas Schmidt <lucas.schmidt015@gmail.com>
2022-03-23 22:03:36 -0300

    add

--- src/br/uffs/cc/jarena/BestAgente.java
+++ src/br/uffs/cc/jarena/BestAgente.java
@@ -1,16 +1,7 @@
-/**
- * Um exemplo de agente que anda aleatoriamente na arena. Esse agente pode ser usado como base
- * para a criação de um agente mais esperto. Para mais informações sobre métodos que podem
- * ser utilizados, veja a classe Agente.java.
- * 
- * Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
- */
+package br.uffs.cc.jarena;
 
-/*package br.uffs.cc.jarena;
-
-public class AgenteDummy extends Agente
-{
-	public AgenteDummy(Integer x, Integer y, Integer energia) {
+public class BestAgente extends Agente{
+    public BestAgente(Integer x, Integer y, Integer energia) {
 		super(x, y, energia);
 		setDirecao(geraDirecaoAleatoria());
 	}
@@ -91,4 +82,3 @@ public class AgenteDummy extends Agente
 		return XY;
 	}
 }
-*/
